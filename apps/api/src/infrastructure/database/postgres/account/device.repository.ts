bf05ce7811702b7f8f91a4ba6eb28e8626aad045
Leandro Santiago Gomes <leandroluk@gmail.com>
2026-02-05 09:32:09 -0300

    fix: wrong repository

--- apps/api/src/infrastructure/database/postgres/account/device.repository.ts
+++ apps/api/src/infrastructure/database/postgres/account/device.repository.ts
@@ -34,7 +34,7 @@ const create = `
     "name",
     "created_at",
     "updated_at"
-  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
 `;
 
 const update = `
